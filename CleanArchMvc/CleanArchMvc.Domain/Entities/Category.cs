ec124e1ff64240b0a746fd8282e40d68ea87d446
Ricardo Carvalho <ricardocarvalhoa@hotmail.com>
2021-04-29 14:25:36 -0300

    Enriquecendo o modelo de domínio criado no commit anterior.

--- CleanArchMvc/CleanArchMvc.Domain/Entities/Category.cs
+++ CleanArchMvc/CleanArchMvc.Domain/Entities/Category.cs
@@ -1,12 +1,37 @@
 ﻿using System.Collections.Generic;
+using CleanArchMvc.Domain.Validation;
 
 namespace CleanArchMvc.Domain.Entities
 {
-    public class Category
+    public sealed class Category
     {
-        public int Id { get; set; }
-        public string Name { get; set; }
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+
+        public Category(string name)
+        {
+            ValidateDomain(name);
+        }
+
+        public Category(int id, string name)
+        {
+            DomainExceptionValidation.When(id < 0, "Invalid Id value.");
+            ValidateDomain(name);
+        }
 
         public ICollection<Product> Products { get; set; }
+
+        private void ValidateDomain(string name)
+        {
+
+            DomainExceptionValidation.When(string.IsNullOrEmpty(name),
+                                           "Invalid name. Name is required.");
+
+            DomainExceptionValidation.When(name.Length < 3,
+                               "Invalid name, too short, minimum 3 charecters.");
+
+            Name = name;
+
+        }
     }
 }

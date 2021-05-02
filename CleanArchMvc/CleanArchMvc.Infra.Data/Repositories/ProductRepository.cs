2ddc43f3c29c1a79fdd24588abb120c1ffd3a290
Ricardo Carvalho <ricardocarvalhoa@hotmail.com>
2021-05-02 16:01:23 -0300

    Refatorando - Removendo código duplicado "DRY".

--- CleanArchMvc/CleanArchMvc.Infra.Data/Repositories/ProductRepository.cs
+++ CleanArchMvc/CleanArchMvc.Infra.Data/Repositories/ProductRepository.cs
@@ -24,7 +24,9 @@ namespace CleanArchMvc.Infra.Data.Repositories
 
         public async Task<Product> GetByIdAsync(int? id)
         {
-            return await _productContext.Products.FindAsync(id);
+            //return await _productContext.Products.FindAsync(id);
+            return await _productContext.Products.Include(c => c.Category)
+                         .SingleOrDefaultAsync(p => p.Id == id);
         }
 
         public Task<IEnumerable<Product>> GetCategoriesAsync()
@@ -32,12 +34,6 @@ namespace CleanArchMvc.Infra.Data.Repositories
             throw new System.NotImplementedException();
         }
 
-        public async Task<Product> GetProductCategoryAsync(int? id)
-        {
-            //eager loading
-            return await _productContext.Products.Include(c => c.Category)
-                .SingleOrDefaultAsync(p => p.Id == id);
-        }
 
         public async Task<IEnumerable<Product>> GetProductsAsync()
         {
